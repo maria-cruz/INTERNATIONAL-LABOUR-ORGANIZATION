5359a7927ab1f8425d9b762b8c90da0e9031ae17
Smaffleo <marcelo.basti@gmail.com>
2021-09-12 16:35:54 +0800

    Apply CSS

--- next.config.js
+++ next.config.js
@@ -4,7 +4,11 @@ const path = require("path");
 module.exports = {
   reactStrictMode: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, "common/styles")],
-    prependData: `@import "~@common/styles/main.scss";`,
+    includePaths: [path.join(__dirname, "styles")],
+    prependData: `
+      @use "~@styles/abstracts/variables.scss";
+      @use "~@styles/abstracts/mixins.scss";
+      @use "~@styles/abstracts/utilities.scss";
+    `,
   },
 };
