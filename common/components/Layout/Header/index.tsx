aa1902023f3ce68ea8227624b2d02fa361cfb529
Smaffleo <marcelo.basti@gmail.com>
2021-09-12 21:27:56 +0800

    Apply Link Styles

--- common/components/Layout/Header/index.tsx
+++ common/components/Layout/Header/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./styles.module.scss";
 import NavLink from "@common/components/NavLink";
+import Button from "antd/lib/button";
 
 interface HeaderProps {
   title?: string;
@@ -8,30 +9,32 @@ interface HeaderProps {
   className?: string;
 }
 
-const Header = ({
-  title = "Untitled",
-  gap = "0rem",
-  className = "",
-}: HeaderProps) => {
+const Header = ({ gap = "0rem", className = "" }: HeaderProps) => {
   return (
     <header
       className={`${styles["header"]} ${className}`}
       style={{ marginBottom: gap }}
     >
-      <div className={styles["left-container"]}>Logo</div>
+      <div className={styles["left-container"]}>
+        <NavLink href="/">Logo</NavLink>
+      </div>
       <div className={styles["right-container"]}>
         <div className={styles["menu"]}>
           <NavLink href="/download-guide">Download Guide</NavLink>
           <NavLink href="/faq">FAQ</NavLink>
           <NavLink href="/about-us">About Us</NavLink>
-          <NavLink href="/">Log In</NavLink>
+          <NavLink className={styles["log-in-link"]} href="/log-in">
+            Log In
+          </NavLink>
           <NavLink href="/sign-up">
-            <button>Sign up</button>
+            <Button type="primary" size="large">
+              Sign up
+            </Button>
           </NavLink>
         </div>
         <div className={styles["divider"]} />
         <div className={styles["language-select"]}>
-          <span> English</span>
+          <span>English</span>
         </div>
       </div>
     </header>
