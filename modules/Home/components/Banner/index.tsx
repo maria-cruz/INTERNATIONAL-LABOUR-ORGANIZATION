216b2c6807d6d431f91acef41c24b38c60c74ad2
Smaffleo <marcelo.basti@gmail.com>
2021-09-14 16:25:11 +0800

    Apply Banner Section

---
+++ modules/Home/components/Banner/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Image from "next/image";
+import Button from "antd/lib/button";
+import styles from "./styles.module.scss";
+
+const Home = () => {
+  return (
+    <section className={styles["banner-section"]}>
+      <span className={styles["banner-title"]}>
+        <b>Rights</b> {"&"} <b>Responsibilities</b> {"at"} <b>Work</b>
+      </span>
+      <span className={styles["banner-description"]}>
+        Houkouki fi al 3amal is an informative and training application on the
+        Lebanese labour laws to better inform workers, employers and greater
+        society on their rights and responsibilities at work. The app features a
+        question and answer related to key aspects of the labour law, a
+        downloadable user-friendly guide/publication, and self-paced training
+        modules.
+      </span>
+
+      <div className={styles["button-container"]}>
+        <Button type="primary" size="middle">
+          Log in
+        </Button>
+        <Button type="default" size="middle">
+          Sign up
+        </Button>
+      </div>
+
+      <div className={styles["watch-link-container"]}>
+        <Image src="/images/play-circle-filled.svg" width={33} height={33} />
+        <a className={styles["watch-link-text"]}>watch how it works</a>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
