d9876b7f8106196d11ebb07cd31394ddc16920c1
Smaffleo <marcelo.basti@gmail.com>
2021-09-14 14:55:22 +0800

    Apply Background

--- modules/Home/index.tsx
+++ modules/Home/index.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import Layout, { Header } from "@common/components/Layout";
+import Image from "next/image";
+import styles from "./styles.module.scss";
 
 const Home = () => {
-  return <Layout header={<Header title={"Header"} />}></Layout>;
+  return (
+    <Layout header={<Header title={"Header"} />}>
+      <div className={styles["background"]}>
+        <Image
+          src="/images/group-cheerful-friends.jpg"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center"
+        />
+      </div>
+    </Layout>
+  );
 };
 
 export default Home;
