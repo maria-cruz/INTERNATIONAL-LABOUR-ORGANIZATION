5f3a1a279896537506c66aca889a47cd89ea6d6b
Smaffleo <marcelo.basti@gmail.com>
2021-09-29 16:18:30 +0800

    Remove

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,8 @@
 import "@styles/main.scss";
-import { Provider } from "next-auth/client";
 import type { AppProps } from "next/app";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return (
-    <Provider session={pageProps.session}>
-      <Component {...pageProps} />
-    </Provider>
-  );
+  return <Component {...pageProps} />;
 };
 
 export default MyApp;
