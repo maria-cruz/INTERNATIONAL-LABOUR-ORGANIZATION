2f71de65c8b4f64e03d23cbc3ccf0b87d3f7c09c
Smaffleo <marcelo.basti@gmail.com>
2021-09-29 14:16:10 +0800

    Apply Strapi Auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import "@styles/main.scss";
+import { Provider } from "next-auth/client";
 import type { AppProps } from "next/app";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <Provider session={pageProps.session}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 };
+
 export default MyApp;
