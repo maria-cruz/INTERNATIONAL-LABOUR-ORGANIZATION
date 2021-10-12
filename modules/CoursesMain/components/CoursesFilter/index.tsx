71898a465085e10341e051913d14a94f54445155
Smaffleo <marcelo.basti@gmail.com>
2021-10-12 10:54:17 +0800

    Apply Query Params instead of Path params

--- modules/CoursesMain/components/CoursesFilter/index.tsx
+++ modules/CoursesMain/components/CoursesFilter/index.tsx
@@ -1,34 +1,33 @@
 import React from "react";
 import { useRouter } from "next/router";
 
+const PATHNAME = "/courses";
+const QUERY_KEY = "category";
+const QUERY_VALUE = {
+  ALL: "all",
+  IN_PROGRESS: "in-progress",
+  COMPLETED: "completed",
+  CERTIFICATE: "certificate",
+};
+
 const CoursesFilter = () => {
   const router = useRouter();
 
-  const isActiveAllUnit = router.query.category === "all";
-  const isActiveInProgress = router.query.category === "in-progress";
-  const isCompleted = router.query.category === "completed";
-  const isCertificate = router.query.category === "certificate";
+  const isActiveAllUnit = router.query[QUERY_KEY] === QUERY_VALUE.ALL;
+  const isActiveInProgress =
+    router.query[QUERY_KEY] === QUERY_VALUE.IN_PROGRESS;
+  const isCompleted = router.query[QUERY_KEY] === QUERY_VALUE.COMPLETED;
+  const isCertificate = router.query[QUERY_KEY] === QUERY_VALUE.CERTIFICATE;
 
-  const handleAllUnitClick = () => {
-    router.push("/courses/all");
+  const handleFilterClick = (category: string) => () => {
+    router.push(`${PATHNAME}?${QUERY_KEY}=${category}`);
   };
 
-  const handleInProgressClick = () => {
-    router.push("/courses/in-progress");
-  };
-
-  const handleCompleteClick = () => {
-    router.push("/courses/completed");
-  };
-
-  const handleCertificateClick = () => {
-    router.push("/courses/certificate");
-  };
   return (
     <div className="courses-filter-container">
       <div
         className={`${isActiveAllUnit ? "-active-filter" : "default-filter"}`}
-        onClick={handleAllUnitClick}
+        onClick={handleFilterClick(QUERY_VALUE.ALL)}
       >
         All Units
       </div>
@@ -36,19 +35,19 @@ const CoursesFilter = () => {
         className={`${
           isActiveInProgress ? "-active-filter" : "default-filter"
         }`}
-        onClick={handleInProgressClick}
+        onClick={handleFilterClick(QUERY_VALUE.IN_PROGRESS)}
       >
         In Progress
       </div>
       <div
         className={`${isCompleted ? "-active-filter" : "default-filter"}`}
-        onClick={handleCompleteClick}
+        onClick={handleFilterClick(QUERY_VALUE.COMPLETED)}
       >
         Completed
       </div>
       <div
         className={`${isCertificate ? "-active-filter" : "default-filter"}`}
-        onClick={handleCertificateClick}
+        onClick={handleFilterClick(QUERY_VALUE.CERTIFICATE)}
       >
         Certificate
       </div>
