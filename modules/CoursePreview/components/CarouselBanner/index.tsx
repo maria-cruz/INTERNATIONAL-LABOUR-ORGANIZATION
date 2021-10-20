0e8d9fd05ba4ef41b46e544414c2f67aa4c20e06
Smaffleo <marcelo.basti@gmail.com>
2021-10-20 20:50:30 +0800

    Apply Course Preview

---
+++ modules/CoursePreview/components/CarouselBanner/index.tsx
@@ -0,0 +1,71 @@
+import Layout, { Header } from "@common/components/Layout";
+import React from "react";
+import Tabs from "antd/lib/tabs";
+import Image from "next/image";
+import Unit1Banner from "@public/images/unit1-banner.jpg";
+import Button from "antd/lib/button";
+import BackArrow from "@common/components/Icons/BackArrow";
+import ForwardArrow from "@common/components/Icons/ForwardArrow";
+import { useRouter } from "next/router";
+
+const { TabPane } = Tabs;
+
+const handleTabsChange = (key: any) => {
+  console.log(key);
+};
+
+const CarouselBanner = () => {
+  const router = useRouter();
+  const handleStartClick = () => {
+    router.push("/courses/view");
+  };
+  const handleUnit2Click = () => {
+    router.push("/courses/unit2");
+  };
+  const handleUnit3Click = () => {
+    router.push("/courses/unit3");
+  };
+
+  return (
+    <div className="carousel-banner-container">
+      <div className="banner-container">
+        <Image
+          src={Unit1Banner}
+          alt="unit1-banner.jpg"
+          placeholder="blur"
+          height={500}
+          width={1920}
+        />
+      </div>
+
+      <div className="bg-filter" />
+
+      <div className="bg-description-container">
+        <div className="unit-label">Unit 1</div>
+
+        <div className="title-container">
+          <div className="back-icon" onClick={handleUnit3Click}>
+            <BackArrow width="3.6rem" height="4.3rem" fill="#8F949D" />
+          </div>
+          <span className="title">
+            The Employment Contract. (Conclusion, Execution and Termination)
+          </span>
+          <div className="forward-icon" onClick={handleUnit2Click}>
+            <ForwardArrow width="3.6rem" height="4.3rem" fill="#8F949D" />
+          </div>
+        </div>
+
+        <Button
+          onClick={handleStartClick}
+          htmlType="submit"
+          type="primary"
+          className="start-btn"
+        >
+          Start Lesson
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default CarouselBanner;
--- modules/CoursePreview/index.tsx
+++ modules/CoursePreview/index.tsx
@@ -1,13 +1,63 @@
 import Layout, { Header } from "@common/components/Layout";
-import Unit1 from "./Unit1";
+// import Unit1 from "./Unit1";
 import React from "react";
 
+import SelfPractice from "./components/SelfPractice";
+import UnitDetails from "./components/UnitDetails";
+import QandA from "./components/Q&A";
+import Tabs from "antd/lib/tabs";
+import Image from "next/image";
+import Unit1Banner from "@public/images/unit1-banner.jpg";
+import Button from "antd/lib/button";
+import BackArrow from "@common/components/Icons/BackArrow";
+import ForwardArrow from "@common/components/Icons/ForwardArrow";
+import { useRouter } from "next/router";
+
+import CarouselBanner from "./components/CarouselBanner";
+
+const { TabPane } = Tabs;
+
+const handleTabsChange = (key: any) => {
+  console.log(key);
+};
+
 const CoursePreview = () => {
+  const router = useRouter();
+  const handleStartClick = () => {
+    router.push("/courses/view");
+  };
+  const handleUnit2Click = () => {
+    router.push("/courses/unit2");
+  };
+  const handleUnit3Click = () => {
+    router.push("/courses/unit3");
+  };
+
   return (
     <Layout header={<Header title={"Header"} />}>
       <section className="course-preview-section">
         <div className="course-details-container">
-          <Unit1 />
+          {
+            <div className="unit-container">
+              <CarouselBanner />
+
+              {/* <Tabs
+                className="tab-container"
+                defaultActiveKey="1"
+                onChange={handleTabsChange}
+              >
+                <TabPane className="tab-item" tab="Unit Details" key="1">
+                  <UnitDetails />
+                </TabPane>
+                <TabPane className="tab-item" tab={"Q & A"} key="2">
+                  <QandA />
+                </TabPane>
+                <TabPane className="tab-item" tab="Self Practice" key="3">
+                  <SelfPractice />
+                </TabPane>
+              </Tabs> */}
+            </div>
+          }
         </div>
       </section>
     </Layout>
