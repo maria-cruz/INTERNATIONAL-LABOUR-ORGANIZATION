07293b6713d953ab157e711c7af85c172c10ab72
Smaffleo <marcelo.basti@gmail.com>
2021-10-28 00:30:20 +0800

    Apply Carousel Sponsor

--- common/components/Partners/index.tsx
+++ common/components/Partners/index.tsx
@@ -10,45 +10,106 @@ import { LeftOutlined, RightOutlined } from "@ant-design/icons";
 const Partners = ({
   subheading = "This app was developed by an ILO project with the following partners",
 }) => {
+  const settings = {
+    dots: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    touchThreshold: 1000,
+    responsive: [
+      {
+        breakpoint: 1800,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <section className="partners-section">
       <span className="partners-section-title">{subheading}</span>
       <div className="partners-section-logos">
-        <div className="carousel-image-container">
-          <Image
-            src={Logo}
-            width={189}
-            height={189}
-            alt="supporters-logo.svg"
-          />
-        </div>
-        <div className="carousel-image-container">
-          <Image
-            src={UnicefLogo}
-            width={289}
-            height={178}
-            placeholder="blur"
-            alt="@public/images/unicef-logo.png"
-          />
-        </div>
-        <div className="carousel-image-container">
-          <Image
-            src={AgencyLogo}
-            width={229}
-            height={189}
-            placeholder="blur"
-            alt="italian-agency-logo.png"
-          />
-        </div>
-        <div className="carousel-image-container">
-          <Image
-            src={IloLogo}
-            width={326}
-            height={117}
-            placeholder="blur"
-            alt="@public/images/ilo-logo.png"
-          />
-        </div>
+        <Carousel {...settings}>
+          <div className="carousel-image-container">
+            <Image
+              src={Logo}
+              width={189}
+              height={189}
+              alt="supporters-logo.svg"
+            />
+          </div>
+          <div className="carousel-image-container">
+            <Image
+              src={UnicefLogo}
+              width={289}
+              height={178}
+              placeholder="blur"
+              alt="@public/images/unicef-logo.png"
+            />
+          </div>
+          <div className="carousel-image-container">
+            <Image
+              src={AgencyLogo}
+              width={229}
+              height={189}
+              placeholder="blur"
+              alt="italian-agency-logo.png"
+            />
+          </div>
+          <div className="carousel-image-container">
+            <Image
+              src={IloLogo}
+              width={326}
+              height={117}
+              placeholder="blur"
+              alt="@public/images/ilo-logo.png"
+            />
+          </div>
+          <div className="carousel-image-container">
+            <Image
+              src={IloLogo}
+              width={326}
+              height={117}
+              placeholder="blur"
+              alt="@public/images/ilo-logo.png"
+            />
+          </div>
+          <div className="carousel-image-container">
+            <Image
+              src={IloLogo}
+              width={326}
+              height={117}
+              placeholder="blur"
+              alt="@public/images/ilo-logo.png"
+            />
+          </div>
+          <div className="carousel-image-container">
+            <Image
+              src={IloLogo}
+              width={326}
+              height={117}
+              placeholder="blur"
+              alt="@public/images/ilo-logo.png"
+            />
+          </div>
+        </Carousel>
       </div>
     </section>
   );
