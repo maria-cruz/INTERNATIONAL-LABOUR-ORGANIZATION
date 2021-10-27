afaf602ce009d0c05baade35ae66ba103dc71ab3
Teresa Cruz <teresa.cruz@ubidy.com>
2021-10-28 00:38:38 +0800

    Apply Download Guide FE

--- pages/download-guide/index.tsx
+++ pages/download-guide/index.tsx
@@ -1,3 +1,61 @@
-import Download from "@modules/DownloadGuide";
+import DownloadGuide from "@modules/DownloadGuide";
+import axios from "axios";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import getStrapiFileUrl from "@common/utils/getStrapiFileUrl";
+interface UnitType {
+  unit: string;
+  title: string;
+  description: string;
+  thumbnail: any;
+}
 
-export default Download;
+interface FileType {
+  url: string;
+}
+interface GuidesDataType {
+  unit: UnitType;
+  file: FileType;
+}
+
+interface AllGuidesDataType {
+  data: GuidesDataType[];
+}
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { data: allDownloadData }: AllGuidesDataType = await axios.get(
+    `${process.env.API_URL}/guides`
+  );
+  console.log(allDownloadData, "alldl");
+
+  let allGuidesData = allDownloadData.map((guideData) => {
+    const guideUnit = guideData?.unit?.unit;
+    const guideTitle = guideData?.unit?.title;
+    const guideDescription = guideData?.unit?.description;
+    const guideThumbnail = getStrapiFileUrl(guideData?.unit?.thumbnail?.url);
+    const guideFile = getStrapiFileUrl(guideData?.file?.url);
+    const isUnitMissing = !guideUnit;
+    if (isUnitMissing) {
+      return null;
+    }
+    return {
+      unit: `Unit ${guideUnit ?? 0}`,
+      title: guideTitle || "",
+      description: guideDescription || "",
+      thumbnail: guideThumbnail,
+      url: guideFile,
+    };
+  });
+
+  allGuidesData = allGuidesData.filter((guideData) => !!guideData);
+  console.log(allGuidesData, "download");
+
+  return {
+    props: { allGuidesData },
+  };
+};
+
+const DownloadGuidePage = ({
+  allGuidesData,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  return <DownloadGuide allGuidesData={allGuidesData} />;
+};
+export default DownloadGuidePage;
