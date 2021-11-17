3d0409dd591decb611e3c54e7cd1863dfb2a16cf
karen <karennipin86@gmail.com>
2021-11-17 16:25:07 +0800

    Apply Hamburger Menu Drawer

--- common/components/HamburgerMenu/index.tsx
+++ common/components/HamburgerMenu/index.tsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import Drawer from "antd/lib/drawer";
+import NavLink from "@common/components/NavLink";
 
 const HamburgerMenu = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const handleHamburgerMenuClick = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
   return (
-    <button className={"hamburger-menu"}>
-      <div className={"line"}></div>
-      <div className={"line"}></div>
-      <div className={"line"}></div>
-    </button>
+    <>
+      <button className={"hamburger-menu"} onClick={handleHamburgerMenuClick}>
+        <div className={"line"}></div>
+        <div className={"line"}></div>
+        <div className={"line"}></div>
+      </button>
+      <Drawer
+        className={"hamburger-menu-drawer"}
+        title="Drawer with extra actions"
+        placement={"top"}
+        visible={isDrawerOpen}
+      >
+        <NavLink href="/sign-up" exact={true}>
+          Sign up
+        </NavLink>
+        <NavLink href="/log-in">Log in</NavLink>
+        <NavLink href="/download-guide">Download Guide</NavLink>
+        <NavLink href="/faq">FAQ</NavLink>
+        <NavLink href="/about-us">About Us</NavLink>
+      </Drawer>
+    </>
   );
 };
 
