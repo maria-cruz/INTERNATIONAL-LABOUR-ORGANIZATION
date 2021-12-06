969f161ceb33f49050eb64dfa501a8197b215011
karen <karennipin86@gmail.com>
2021-12-06 23:09:10 +0800

    Remove Console

--- pages/courses/preview/[slug]/index.tsx
+++ pages/courses/preview/[slug]/index.tsx
@@ -59,11 +59,6 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     }
   );
 
-  console.log(
-    currentCourseData?.self_practice,
-    " currentCourseData?.self_practice"
-  );
-
   const prevCourse = allCoursesData
     ?.slice?.()
     ?.reverse?.()
