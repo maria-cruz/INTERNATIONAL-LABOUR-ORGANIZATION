774b23ffb0de1e947959cdd01fa586d0aec8d719
karen <karennipin86@gmail.com>
2021-12-09 01:23:56 +0800

    Fix QandA for both preview and view

--- common/components/CourseTabs/Q&A/index.tsx
+++ common/components/CourseTabs/Q&A/index.tsx
@@ -130,124 +130,129 @@ const QandA = ({ courseId, courseComments }: QandAProps) => {
 
   return (
     <section className="QandA-section">
-      <div className="comment-form-wrapper">
-        <Form form={qAndAForm} onFinish={handleQAndAFinish}>
-          <div className="comment-container">
-            <Form.Item name="comment" label={`${t("askHere")}`}>
-              <TextArea className="text-input-container" />
-            </Form.Item>
-            <Button htmlType="submit" type="primary" className="submit-btn">
+      <div className="qanda-container">
+        <div className="comment-form-wrapper">
+          <Form form={qAndAForm} onFinish={handleQAndAFinish}>
+            <div className="comment-container">
+              <Form.Item name="comment" label={`${t("askHere")}`}>
+                <TextArea className="text-input-container" />
+              </Form.Item>
+              <Button htmlType="submit" type="primary" className="submit-btn">
+                {t("askQuestion")}
+              </Button>
+            </div>
+
+            <Button
+              htmlType="submit"
+              type="primary"
+              className="submit-btn-mobile"
+            >
               {t("askQuestion")}
             </Button>
-          </div>
+          </Form>
+        </div>
 
-          <Button
-            htmlType="submit"
-            type="primary"
-            className="submit-btn-mobile"
-          >
-            {t("askQuestion")}
-          </Button>
-        </Form>
-      </div>
+        {comments?.map?.((comment, idx) => {
+          const commentId = comment?.id;
 
-      {comments?.map?.((comment, idx) => {
-        const commentId = comment?.id;
+          const hasName =
+            !!comment?.authorUser?.given_name &&
+            !!comment?.authorUser?.family_name;
+          const hasOrganization = !!comment?.authorUser?.organization_type;
+          const hasContent = !!comment?.content;
 
-        const hasName =
-          !!comment?.authorUser?.given_name &&
-          !!comment?.authorUser?.family_name;
-        const hasOrganization = !!comment?.authorUser?.organization_type;
-        const hasContent = !!comment?.content;
+          const shouldNotRenderComment = !(
+            hasName &&
+            hasOrganization &&
+            hasContent
+          );
+          if (shouldNotRenderComment) return null;
 
-        const shouldNotRenderComment = !(
-          hasName &&
-          hasOrganization &&
-          hasContent
-        );
-        if (shouldNotRenderComment) return null;
+          const fullName = hasName
+            ? `${comment?.authorUser?.given_name} ${comment?.authorUser?.family_name}`
+            : undefined;
 
-        const fullName = hasName
-          ? `${comment?.authorUser?.given_name} ${comment?.authorUser?.family_name}`
-          : undefined;
+          const checkOrganizationType = SAMPLE_DATA_ORGANIZATION_TYPE.find(
+            (item) => item.value === comment?.authorUser?.organization_type
+          );
 
-        const checkOrganizationType = SAMPLE_DATA_ORGANIZATION_TYPE.find(
-          (item) => item.value === comment?.authorUser?.organization_type
-        );
+          const organizationType = checkOrganizationType?.key
+            ? checkOrganizationType.key
+            : "";
 
-        const organizationType = checkOrganizationType?.key
-          ? checkOrganizationType.key
-          : "";
+          return (
+            <div key={`comment-${idx}`}>
+              <div className="comment-display-container">
+                <div className="name-container">{fullName}</div>
+                <div className="description-container">
+                  {t(organizationType)}
+                </div>
+                <p className="question-display-container">{comment?.content}</p>
+              </div>
+              <div className="conversation-container">
+                {comment?.children?.map?.((reply, idx) => {
+                  const hasName =
+                    !!reply?.authorUser?.given_name &&
+                    !!reply?.authorUser?.family_name;
+                  const hasOrganization =
+                    !!reply?.authorUser?.organization_type;
+                  const hasContent = !!reply?.content;
 
-        return (
-          <div key={`comment-${idx}`}>
-            <div className="comment-display-container">
-              <div className="name-container">{fullName}</div>
-              <div className="description-container">{t(organizationType)}</div>
-              <p className="question-display-container">{comment?.content}</p>
-            </div>
-            <div className="conversation-container">
-              {comment?.children?.map?.((reply, idx) => {
-                const hasName =
-                  !!reply?.authorUser?.given_name &&
-                  !!reply?.authorUser?.family_name;
-                const hasOrganization = !!reply?.authorUser?.organization_type;
-                const hasContent = !!reply?.content;
-
-                const shouldNotRenderreply = !(
-                  hasName &&
-                  hasOrganization &&
-                  hasContent
-                );
-                if (shouldNotRenderreply) return null;
-
-                const fullName = hasName
-                  ? `${reply?.authorUser?.given_name} ${reply?.authorUser?.family_name}`
-                  : undefined;
-
-                const checkRepyOrganizationType =
-                  SAMPLE_DATA_ORGANIZATION_TYPE.find(
-                    (item) =>
-                      item.value === reply?.authorUser?.organization_type
+                  const shouldNotRenderreply = !(
+                    hasName &&
+                    hasOrganization &&
+                    hasContent
                   );
+                  if (shouldNotRenderreply) return null;
 
-                const replyOrganizationType = checkRepyOrganizationType?.key
-                  ? checkRepyOrganizationType.key
-                  : "";
+                  const fullName = hasName
+                    ? `${reply?.authorUser?.given_name} ${reply?.authorUser?.family_name}`
+                    : undefined;
 
-                return (
-                  <div className="item-wrapper" key={`reply-${idx}`}>
-                    <div className="name-container">{fullName}</div>
-                    <div className="label-container">
-                      {t(replyOrganizationType)}
+                  const checkRepyOrganizationType =
+                    SAMPLE_DATA_ORGANIZATION_TYPE.find(
+                      (item) =>
+                        item.value === reply?.authorUser?.organization_type
+                    );
+
+                  const replyOrganizationType = checkRepyOrganizationType?.key
+                    ? checkRepyOrganizationType.key
+                    : "";
+
+                  return (
+                    <div className="item-wrapper" key={`reply-${idx}`}>
+                      <div className="name-container">{fullName}</div>
+                      <div className="label-container">
+                        {t(replyOrganizationType)}
+                      </div>
+                      <p className="question-display-container">
+                        {reply?.content}
+                      </p>
                     </div>
-                    <p className="question-display-container">
-                      {reply?.content}
-                    </p>
-                  </div>
-                );
-              })}
+                  );
+                })}
 
-              <div className="reply-container">
-                <Form.Item className="reply-container">
-                  <TextArea
-                    className="reply-input-container"
-                    placeholder={`${t("comment")}`}
-                    value={replyValues[idx]}
-                    onChange={handleTextAreaChange(idx)}
-                  />
-                </Form.Item>
-                <Button
-                  onClick={handleReplyFinish(commentId, idx)}
-                  className="reply-btn"
-                >
-                  {t("reply")}
-                </Button>
+                <div className="reply-container">
+                  <Form.Item className="reply-container">
+                    <TextArea
+                      className="reply-input-container"
+                      placeholder={`${t("comment")}`}
+                      value={replyValues[idx]}
+                      onChange={handleTextAreaChange(idx)}
+                    />
+                  </Form.Item>
+                  <Button
+                    onClick={handleReplyFinish(commentId, idx)}
+                    className="reply-btn"
+                  >
+                    {t("reply")}
+                  </Button>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </section>
   );
 };
